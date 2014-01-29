b2c8af1e28dc15e44c5c1caee4219930d0f3d97b
landtax <santiago.bel@webfordesigners.com>
2014-01-29 22:57:45 +0100

    fix bug in install

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-ls -sf ./tmux.conf ~/.tmux.conf
-ls -sf ./vim/vimrc ~/.vimrc
-ls -sf ./vim/gvimrc ~/.gvimrc
+ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
+ln -sf ~/dotfiles/vim/gvimrc ~/.gvimrc
