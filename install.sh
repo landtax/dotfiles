82fd035c55c6230ed7c28a00526de29139c852e2
Santi <santiago.bel@webfordesigners.com>
2014-01-31 18:37:25 +0100

    split tmux configurations MAC/UBUNTU

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
 ln -sf ~/dotfiles/vim/gvimrc ~/.gvimrc
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  ln -sf ~/dotfiles/tmux.conf.mac ~/.tmux.conf.local
+else
+  ln -sf ~/dotfiles/tmux.conf.ubuntu ~/.tmux.conf.local
+fi
