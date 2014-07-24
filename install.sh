8c32f62bf1dd784c87496ff8df9eca5465cf80cc
landtax <santiago.bel@webfordesigners.com>
2014-07-24 22:02:15 +0200

    added ack configuration an ag plugin for vim

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
 ln -sf ~/dotfiles/vim/gvimrc ~/.gvimrc
+ln -sf ~/dotfiles/vim/ackrc ~/.ackrc
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
   ln -sf ~/dotfiles/tmux.conf.mac ~/.tmux.conf.local
